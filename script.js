f796f81d117ba08c5ed3a5182cafdcaec7d5c18c
Greyson Knight <greyson.r.knight@gmail.com>
2024-03-09 13:28:47 -0500

    set up and finalized play() function

--- script.js
+++ script.js
@@ -10,9 +10,35 @@ function getComputerChoice() {
     }
 
     return choice;
-
 }
 
 
+function play(playerSelection, computerSelection) {
 
-console.log(getComputerChoice());
+    displayChoices();
+    let result;
+
+    if (playerSelection === computerSelection) {
+        result = "Tie";
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors')
+                || (playerSelection === 'scissors' && computerSelection === 'paper')
+                || (playerSelection === 'paper' && computerSelection === 'rock')) {
+                    result = "You win";
+    } else {
+        result = "You lose";
+    }
+
+    return result;
+}
+
+
+function displayChoices() {
+    console.log("Your selection: " + playerChoice);
+    console.log("Computer selection: " + computerChoice);
+}
+
+
+let computerChoice = getComputerChoice();
+let playerChoice = (prompt("Please enter your selection").toLowerCase());
+
+console.log(play(playerChoice, computerChoice));
